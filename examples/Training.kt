b79ca3fd966b67ccf55a60ce90a8decd3a26616f
KotlinNLP Authors <github@kotlinnlp.com>
2018-10-03 22:39:33 +0200

    Set mainBody block

--- examples/Training.kt
+++ examples/Training.kt
@@ -24,6 +24,7 @@ import com.kotlinnlp.simplednn.core.embeddings.EMBDLoader
 import com.kotlinnlp.simplednn.core.functionalities.updatemethods.adam.ADAMMethod
 import com.kotlinnlp.simplednn.core.layers.LayerType
 import com.kotlinnlp.tokensencoder.TokensEncoder
+import com.xenomachina.argparser.mainBody
 import java.io.File
 import java.io.FileInputStream
 
@@ -32,7 +33,7 @@ import java.io.FileInputStream
  *
  * Launch with the '-h' option for help about the command line arguments.
  */
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = mainBody {
 
   val parsedArgs = CommandLineArguments(args)
 
