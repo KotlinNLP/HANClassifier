c0fd99815dccc4b2ef33874b93ee79781e2bbb8c
KotlinNLP Authors <github@kotlinnlp.com>
2018-12-17 12:50:17 +0100

    Add evaluation example

---
+++ examples/evaluation/CommandLineArguments.kt
@@ -0,0 +1,48 @@
+/* Copyright 2016-present KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
+package evaluation
+
+import com.xenomachina.argparser.ArgParser
+
+/**
+ * The interpreter of command line arguments.
+ *
+ * @param args the array of command line arguments
+ */
+internal class CommandLineArguments(args: Array<String>) {
+
+  /**
+   * The parser of the string arguments.
+   */
+  private val parser = ArgParser(args)
+
+  /**
+   * The name of the model.
+   */
+  val modelName: String by parser.storing(
+    "-n",
+    "--model-name",
+    help="the name of the model"
+  )
+
+  /**
+   * The file path of the validation dataset.
+   */
+  val validationSetPath: String by parser.storing(
+    "-v",
+    "--validation-set-path",
+    help="the file path of the validation dataset"
+  )
+
+  /**
+   * Force parsing all arguments (only read ones are parsed by default).
+   */
+  init {
+    parser.force()
+  }
+}
---
+++ examples/evaluation/Evaluate.kt
@@ -0,0 +1,44 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package evaluation
+
+import com.kotlinnlp.hanclassifier.HANClassifierModel
+import com.kotlinnlp.hanclassifier.dataset.CorpusReader
+import com.kotlinnlp.hanclassifier.dataset.Example
+import com.kotlinnlp.hanclassifier.helpers.Validator
+import com.xenomachina.argparser.mainBody
+import java.io.File
+import java.io.FileInputStream
+
+/**
+ * Evaluate a [HANClassifierModel].
+ *
+ * Launch with the '-h' option for help about the command line arguments.
+ */
+fun main(args: Array<String>) = mainBody {
+
+  val parsedArgs = CommandLineArguments(args)
+
+  val validationSet: List<Example> = parsedArgs.validationSetPath.let {
+    println("Loading validation dataset from '$it'...")
+    CorpusReader().read(it)
+  }
+
+  val model = HANClassifierModel.load(FileInputStream(File(parsedArgs.modelName)))
+
+  println("\n-- START VALIDATION ON %d SENTENCES".format(validationSet.size))
+
+  val info: Validator.ValidationInfo = Validator(model).validate(testSet = validationSet)
+  val accuracy: Double = info.metrics.map { it.f1Score }.average()
+
+  println("Accuracy (f1 average): %5.2f %%".format(100 * accuracy))
+  info.metrics.forEachIndexed { i, it -> println("- Level $i: $it") }
+
+  println("Level 0 confusion:")
+  println(info.confusionMatrix)
+}
