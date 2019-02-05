7da5dba518af299d6781e63b83dfd145d980bc4e
KotlinNLP Authors <github@kotlinnlp.com>
2019-02-05 13:10:10 +0100

    Add example 'EvaluateSeparateModels'

---
+++ examples/evaluation/separatemodels/EvaluateSeparateModels.kt
@@ -0,0 +1,59 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package evaluation.separatemodels
+
+import com.kotlinnlp.hanclassifier.HANClassifierModel
+import com.kotlinnlp.hanclassifier.MultiLevelHANModel
+import com.kotlinnlp.hanclassifier.dataset.CorpusReader
+import com.kotlinnlp.hanclassifier.dataset.Example
+import com.kotlinnlp.hanclassifier.helpers.Validator
+import com.kotlinnlp.simplednn.core.embeddings.EMBDLoader
+import com.kotlinnlp.tokensencoder.embeddings.EmbeddingsEncoderModel
+import com.kotlinnlp.tokensencoder.embeddings.keyextractor.NormWordKeyExtractor
+import com.xenomachina.argparser.mainBody
+import java.io.File
+import java.io.FileInputStream
+
+/**
+ * Evaluate a [HANClassifierModel] loading the multi-level classifiers model and the embeddings separately.
+ *
+ * Launch with the '-h' option for help about the command line arguments.
+ */
+fun main(args: Array<String>) = mainBody {
+
+  val parsedArgs = CommandLineArguments(args)
+
+  val classifiersModel: MultiLevelHANModel = parsedArgs.classifiersModelPath.let {
+    println("Loading multi-level classifiers model from '$it'...")
+    MultiLevelHANModel.load(FileInputStream(File(it)))
+  }
+  val tokensEncoderModel = EmbeddingsEncoderModel(
+    embeddingsMap = parsedArgs.embeddingsPath.let {
+      println("Loading embeddings from '$it'...")
+      EMBDLoader().load(it)
+    },
+    embeddingKeyExtractor = NormWordKeyExtractor())
+  val validationSet: List<Example> = parsedArgs.validationSetPath.let {
+    println("Loading validation dataset from '$it'...")
+    CorpusReader().read(it)
+  }
+
+  println("\n-- START VALIDATION ON %d SENTENCES".format(validationSet.size))
+
+  val model = HANClassifierModel(multiLevelHAN = classifiersModel, tokensEncoder = tokensEncoderModel)
+  val info: Validator.ValidationInfo = Validator(model).validate(testSet = validationSet)
+  val accuracy: Double = info.metrics.map { it.f1Score }.average()
+
+  println()
+  println("Accuracy (f1 average): %5.2f %%".format(100 * accuracy))
+  info.metrics.forEachIndexed { i, it -> println("- Level $i: $it") }
+
+  println()
+  println("Level 0 confusion:")
+  println(info.confusionMatrix)
+}
