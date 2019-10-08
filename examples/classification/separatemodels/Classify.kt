bb0a912f9b7063ec64e21f7e6300053aab65e928
KotlinNLP Authors <github@kotlinnlp.com>
2019-10-08 12:50:59 +0200

    Add package 'classification.separatemodels'

---
+++ examples/classification/separatemodels/Classify.kt
@@ -0,0 +1,116 @@
+/* Copyright 2016-present The KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ * ------------------------------------------------------------------*/
+
+package classification.separatemodels
+
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.kotlinnlp.hanclassifier.HANClassifier
+import com.kotlinnlp.hanclassifier.HANClassifierModel
+import com.kotlinnlp.hanclassifier.LabelsConfig
+import com.kotlinnlp.linguisticdescription.sentence.Sentence
+import com.kotlinnlp.linguisticdescription.sentence.token.FormToken
+import com.kotlinnlp.neuraltokenizer.NeuralTokenizer
+import com.kotlinnlp.neuraltokenizer.NeuralTokenizerModel
+import com.kotlinnlp.simplednn.core.embeddings.EmbeddingsMap
+import com.kotlinnlp.simplednn.simplemath.ndarray.dense.DenseNDArray
+import com.kotlinnlp.tokensencoder.embeddings.EmbeddingsEncoderModel
+import com.kotlinnlp.tokensencoder.reduction.ReductionEncoderModel
+import com.xenomachina.argparser.mainBody
+import java.io.File
+import java.io.FileInputStream
+
+/**
+ * Classify texts from standard input with a transient embeddings encoder, loading the embeddings separately.
+ *
+ * Launch with the '-h' option for help about the command line arguments.
+ */
+fun main(args: Array<String>) = mainBody {
+
+  val parsedArgs = CommandLineArguments(args)
+
+  val model: HANClassifierModel = parsedArgs.classifierModelPath.let {
+    println("Loading HAN classifier model from '$it'...")
+    HANClassifierModel.load(FileInputStream(File(it)))
+  }
+
+  val embeddingsMap: EmbeddingsMap<String> = parsedArgs.embeddingsPath.let {
+    println("Loading pre-trained word embeddings from '$it'...")
+    EmbeddingsMap.load(it)
+  }
+
+  val inputTokensEncoder: EmbeddingsEncoderModel.Transient<*, *> =
+    (model.tokensEncoder as ReductionEncoderModel).inputEncoderModel as EmbeddingsEncoderModel.Transient
+
+  inputTokensEncoder.setEmbeddingsMap(embeddingsMap)
+
+  val classifier = HANClassifier(model)
+
+  val tokenizer = NeuralTokenizer(model = parsedArgs.tokenizerModelPath.let {
+    println("Loading tokenizer model from '$it'...")
+    NeuralTokenizerModel.load(FileInputStream(File(it)))
+  })
+
+  val labelsConfig: LabelsConfig? = parsedArgs.labelsConfigPath?.let {
+    println("Loading labels configuration from '$it'...")
+    LabelsConfig.fromJSON(Parser().parse(it) as JsonObject)
+  }
+
+  var inputText = readInput()
+
+  while (inputText.isNotEmpty()) {
+
+    @Suppress("UNCHECKED_CAST")
+    val sentences: List<Sentence<FormToken>> = tokenizer.tokenize(inputText).map { it as Sentence<FormToken> }
+    val sentencesToUse: List<Sentence<FormToken>> = if (parsedArgs.reduceSentences) reduce(sentences) else sentences
+    val predictions: List<DenseNDArray> = classifier.classify(sentencesToUse)
+    val confidencePerLevel: List<Double> = predictions.map { it.max() }
+    val predictedClass: String =
+      labelsConfig?.getLabel(predictions) ?: predictions.joinToString("-") { it.argMaxIndex().toString() }
+
+    var accuracy = 1.0
+    confidencePerLevel.forEach { accuracy *= it }
+
+    println()
+    println("Predicted class: '$predictedClass' (level: ${predictions.size})")
+    println("Global confidence: %.1f%% (%s)"
+      .format(100.0 * accuracy, confidencePerLevel.joinToString(" x ") { "%.1f%%".format(100.0 * it) }))
+
+    inputText = readInput()
+  }
+
+  println("Done.")
+}
+
+/**
+ * Read a text from the standard input.
+ *
+ * @return the string read
+ */
+private fun readInput(): String {
+
+  print("\nClassify a text (empty to exit): ")
+
+  return readLine()!!.trim()
+}
+
+/**
+ * Reduce the sentences to the first two with at least 5 tokens.
+ *
+ * @param sentences a list of sentences
+ *
+ * @return a list of reduced sentences
+ */
+private fun reduce(sentences: List<Sentence<FormToken>>): List<Sentence<FormToken>> {
+
+  var count = 0
+
+  return sentences.takeWhile {
+    if (it.tokens.size >= 5) count++
+    count <= 2
+  }
+}
---
+++ examples/classification/separatemodels/CommandLineArguments.kt
@@ -0,0 +1,76 @@
+/* Copyright 2016-present KotlinNLP Authors. All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * -----------------------------------------------------------------------------*/
+
+package classification.separatemodels
+
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+
+/**
+ * The interpreter of command line arguments.
+ *
+ * @param args the array of command line arguments
+ */
+internal class CommandLineArguments(args: Array<String>) {
+
+  /**
+   * The parser of the string arguments.
+   */
+  private val parser = ArgParser(args)
+
+  /**
+   * The file path of the HAN classifier serialized model.
+   */
+  val classifierModelPath: String by parser.storing(
+    "-m",
+    "--classifier-model-path",
+    help="the file path of the HAN classifier serialized model"
+  )
+
+  /**
+   * The file path of the pre-trained word embeddings.
+   */
+  val embeddingsPath: String by parser.storing(
+    "-e",
+    "--pre-trained-word-emb-path",
+    help="the file path of the pre-trained word embeddings"
+  )
+
+  /**
+   * The file path of the tokenizer serialized model.
+   */
+  val tokenizerModelPath: String by parser.storing(
+    "-t",
+    "--tokenizer-model-path",
+    help="the file path of the tokenizer serialized model"
+  )
+
+  /**
+   * Reduce the size of the sentences to use for the classification.
+   */
+  val reduceSentences: Boolean by parser.flagging(
+    "-r",
+    "--reduce-sentences",
+    help="reduce the size of the sentences to use for the classification"
+  ).default { false }
+
+  /**
+   * The file path of the labels configuration.
+   */
+  val labelsConfigPath: String? by parser.storing(
+    "-l",
+    "--labels-config-path",
+    help="the file path of the labels configuration"
+  ).default { null }
+
+  /**
+   * Force parsing all arguments (only read ones are parsed by default).
+   */
+  init {
+    parser.force()
+  }
+}
